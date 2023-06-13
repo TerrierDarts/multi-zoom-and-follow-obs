e7704f5047291edf4bf36056f8e31df2666acdeb
TerrierDarts <terrierdarts@gmail.com>
2023-06-13 22:06:03 +0100

    Removed Hard Coding of Canvas

--- README.md
+++ README.md
@@ -7,11 +7,17 @@ Once added to OBS and you will need to add a `Crop/Pad` Filter to the source you
 
 ## TO DO
 
-- Remove the Hardcoding of my Source
 - Add Following to track correctly
 - Auto Turn off follow when zoom is turned off
 - Turn on Follow if going from Off to On
-- Make sure the when going from 2 > 4 or 4 > 2 if follow is off it zoooms correctly 
+- Make sure the when going from 2 > 4 or 4 > 2 if follow is off it zoooms correctly
+
+Problems with the Follow
+
+1 - I need to workout which screen is been captured, as the limits will be different on each screen,
+2 - I need to correctly work out the limits depending on what zoom level is active
+3 - (easy) If Zoom is off follow turns off so nothing moves and set x/y to 0
+4 - if zoom is off and i zoom in or out i dont want it getting thrown off
 
 
 ## OBSRaw Hotkeys (WEBSOCKET 5)
--- multi_level_zoom_and_follow.py
+++ multi_level_zoom_and_follow.py
@@ -3,12 +3,14 @@ import obspython as obs
 import pywinctl as pwc
 import time
 
-
 description = (
     "ZOOM AND FOLLOW"
    
 )
 source = ""
+cWidth = ""
+cHeight = ""
+follow_on = False
 
 
 def populate_list_property_with_source_names(list_property):
@@ -30,65 +32,79 @@ def populate_list_property_with_source_names(list_property):
                 obs.obs_property_list_add_string(list_property, name_val, name)
     obs.source_list_release(sources)
 
+
 def script_properties():
     props = obs.obs_properties_create()
     source_property = obs.obs_properties_add_list(props, "source", "Source", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     populate_list_property_with_source_names(source_property)
     obs.obs_properties_add_button(props, "refresh_sources", "Refresh Sources", populate_list_property_with_source_names)
-
-  
-
+    obs.obs_properties_add_int(props, "width", "Canvas Width", 0, 9999, 1)
+    obs.obs_properties_add_int(props, "height", "Canvas Height",0,9999,1)
 
     return props
 
+
 def script_update(settings):
-    #populate_list_property_with_source_names((obs.obs_properties_get(settings)))
+    # populate_list_property_with_source_names((obs.obs_properties_get(settings)))
     global source
     source = obs.obs_data_get_string(settings, "source")
+    global cWidth
+    cWidth = obs.obs_data_get_int(settings, "width")
+    global cHeight
+    cHeight = obs.obs_data_get_int(settings, "height")
     log(source)
+
 
 def script_description():
     return description
+
 
 def script_load(settings):
     zoomoff = obs.obs_hotkey_register_frontend("zoom_off", "Turn Zoom Off", zoom_off)
     zoomtwo = obs.obs_hotkey_register_frontend("zoom_two", "Zoom Two", zoom_two)
     zoomfour = obs.obs_hotkey_register_frontend("zoom_four", "Zoom Four", zoom_four)
-    followtoggle = obs.obs_hotkey_register_frontend("follow_toggle", "Follow Toggle",follow_toggle)
-    hotkey_save_array = obs.obs_data_get_array(settings,"ZOOM/FOLLOW")
+    followtoggle = obs.obs_hotkey_register_frontend("follow_toggle", "Follow Toggle", follow_toggle)
+    hotkey_save_array = obs.obs_data_get_array(settings, "ZOOM/FOLLOW")
     obs.obs_hotkey_load(zoomoff, hotkey_save_array)
     obs.obs_hotkey_load(zoomtwo, hotkey_save_array)
     obs.obs_hotkey_load(zoomfour, hotkey_save_array)
     obs.obs_hotkey_load(followtoggle, hotkey_save_array)
     obs.obs_data_array_release(hotkey_save_array)
 
+
 def get_cursor_position():
     return pwc.getMousePos()
-
-
   
 
 def log(message):
     print("[LOG INFO] " + str(message))
 
+
 def zoom_off(pressed):
     if pressed:
         curX = get_width()
         curY = get_height()
-        increase_zoom(curX,curY,1920,1080)
+        global cWidth
+        global cHeight
+        increase_zoom(curX, curY, cWidth, cHeight)
+
 
 def zoom_two(pressed):
     if pressed:
         curX = get_width()
         curY = get_height()
-        log(f"zoom2 {curX} {curY}")
-        increase_zoom(curX,curY,960,540)
+        global cWidth
+        global cHeight
+        increase_zoom(curX, curY, cWidth/2, cHeight/2)
+
 
 def zoom_four(pressed):
     if pressed:
         curX = get_width()
         curY = get_height()
-        increase_zoom(curX,curY,480,270)
+        global cWidth
+        global cHeight
+        increase_zoom(curX, curY, cWidth/4, cHeight/4)
 
 
 def get_selected_source():
@@ -98,24 +114,23 @@ def get_selected_source():
     sourceName = text[0]
     return sourceName
 
-def obs_set_crop_settings(width,height):
+
+def obs_set_crop_settings(width, height):
     sourceN = obs.obs_get_source_by_name(get_selected_source())
     crop = obs.obs_source_get_filter_by_name(sourceN, "MultiZoom")
     crop_settings = obs.obs_source_get_settings(crop)
-    log(obs.obs_data_get_json(crop_settings))
+    #log(obs.obs_data_get_json(crop_settings))
     obs.obs_data_set_int(crop_settings, "cx", width)
     obs.obs_data_set_int(crop_settings, "cy", height)
     obs.obs_data_set_bool(crop_settings, "relative", False)
     obs.obs_source_update(crop, crop_settings)
-      
-   
 
 
 def get_height():
     sourceN = obs.obs_get_source_by_name(get_selected_source())
     crop = obs.obs_source_get_filter_by_name(sourceN, "MultiZoom")
     crop_settings = obs.obs_source_get_settings(crop)
-    log(obs.obs_data_get_json(crop_settings))
+    #log(obs.obs_data_get_json(crop_settings))
     curY = obs.obs_data_get_int(crop_settings, "cy")
     return curY
 
@@ -124,7 +139,7 @@ def get_width():
     sourceN = obs.obs_get_source_by_name(get_selected_source())
     crop = obs.obs_source_get_filter_by_name(sourceN, "MultiZoom")
     crop_settings = obs.obs_source_get_settings(crop)
-    log(obs.obs_data_get_json(crop_settings))
+    #log(obs.obs_data_get_json(crop_settings))
     curX = obs.obs_data_get_int(crop_settings, "cx")
     return curX
 
@@ -148,52 +163,52 @@ def increase_zoom(cx, cy, nx, ny):
             cy += 6
             if cy > ny:
                 cy = ny
-        obs_set_crop_settings(cx,cy)
-        
+        obs_set_crop_settings(cx, cy)
 
         time.sleep(0.01)
-    log("Zoom Down")
-
+    #log("Zoom Down")
 
    
 def follow_toggle(pressed):
     if pressed:
+        global follow_on
+        if follow_on is True:
+            follow_on = False
+        else:
+            follow_on = True
+    log(follow_on)
+
+
+def script_tick(seconds):
+    global follow_on
+    if follow_on is True: 
+        # Get mouse Pos          
         mouse = pwc.getMousePos()
         log(f"PosMouseX/{mouse.x} PosMouseY{mouse.y}")
-        #Get mouse Pos
+        
         # Workout level of Zoom
         # adjust level of tracking
         # reset the settings
         sourceName = get_selected_source()
-        #sourceName = obs.obs_get_source_by_name(get_selected_source())
-       
-        
         crop = obs.obs_source_get_filter_by_name(sourceName, "MultiZoom")
         crop_settings = obs.obs_source_get_settings(crop)
         log(obs.obs_data_get_json(crop_settings))
-    
-        curX = obs.obs_data_get_int(crop_settings,"cx")
-        curY = obs.obs_data_get_int(crop_settings,"cy")
+        curX = obs.obs_data_get_int(crop_settings, "cx")
+        curY = obs.obs_data_get_int(crop_settings, "cy")
         log(f"MouseX{curX} MouseY{curY}")
-
         mouseX = 1920 if mouse.x > 1920 else mouse.x
-        mouseY = 1080 if mouse.x > 1080 else mouse.x
-
-        
-        newX = round_up(mouseX -(curX/2))
-        newY = round_up(mouseY -(curY/2))
+        mouseY = 1080 if mouse.x > 1080 else mouse.x      
+        newX = round_up(mouseX - (curX / 2))
+        newY = round_up(mouseY - (curY / 2))
         log(f"NewMouseX{mouseX}/ {newX} NewMouseY {mouseY}/{newY}")
-
         obs.obs_data_set_int(crop_settings, "top", newY)
         obs.obs_data_set_int(crop_settings, "left", newX)
-        obs.obs_data_set_bool(crop_settings, "relative",False)
-    
-        obs.obs_source_update(crop,crop_settings)
-        
+        obs.obs_data_set_bool(crop_settings, "relative", False)
+        obs.obs_source_update(crop, crop_settings) 
         obs.obs_data_release(crop_settings)
         obs.obs_source_release(sourceName)
         obs.obs_source_release(crop)
-        
+            
 
 def round_up(n):
    
